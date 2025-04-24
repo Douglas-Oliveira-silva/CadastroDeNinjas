584975b9e132f984dfcc5d333dfa87fbcfbad37f
Douglas-Oliveira-silva <douglasoliveira.dasilva@hotmail.com>
2025-04-24 20:06:10 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesModel {
     /*ID , Nome missao, Dificuldade (rank)*/
 
@@ -20,36 +26,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private NinjaModel ninja;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String rank, NinjaModel ninja) {
-        this.nome = nome;
-        Rank = rank;
-        this.ninja = ninja;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRank() {
-        return Rank;
-    }
-
-    public void setRank(String rank) {
-        Rank = rank;
-    }
-
-    public NinjaModel getNinja() {
-        return ninja;
-    }
-
-    public void setNinja(NinjaModel ninja) {
-        this.ninja = ninja;
-    }
 }
